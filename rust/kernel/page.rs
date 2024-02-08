b207e23b0cfbe968218e8e6025eedf8325a97c8b
Andreas Hindborg <a.hindborg@samsung.com>
2024-02-08 13:40:54 +0100

    rust: page: add `from_raw()`
    Add a method to `Page` that allows construction of an instance from `struct
    page` pointer.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@samsung.com>

--- rust/kernel/page.rs
+++ rust/kernel/page.rs
@@ -83,6 +83,17 @@ impl Page {
         Opaque::raw_get(&self.page)
     }
 
+    /// Create a `&Page` from a raw `struct page` pointer
+    ///
+    /// # Safety
+    ///
+    /// `ptr` must be valid for use as a reference for the duration of `'a`.
+    pub unsafe fn from_raw<'a>(ptr: *mut bindings::page) -> &'a Self {
+            // SAFETY: By function safety requirements, ptr is not null and is
+            // valid for use as a reference.
+            unsafe { NonNull::new_unchecked(ptr).cast().as_ref() }
+    }
+
     /// Runs a piece of code with this page mapped to an address.
     ///
     /// The page is unmapped when this call returns.
