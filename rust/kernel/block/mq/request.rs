cae0d15f0a03f78814301eccd416fd1a24796475
Andreas Hindborg <a.hindborg@samsung.com>
2024-06-13 11:30:51 +0200

    rust: block: add `command` getter to `Request`

--- rust/kernel/block/mq/request.rs
+++ rust/kernel/block/mq/request.rs
@@ -82,6 +82,12 @@ impl<T: Operations> Request<T> {
         unsafe { ARef::from_raw(NonNull::new_unchecked(ptr as *const Self as *mut Self)) }
     }
 
+    /// Get the command identifier for the request
+    pub fn command(&self) -> u32 {
+        // SAFETY: By C API contract and type invariant, `cmd_flags` is valid for read
+        unsafe { (*self.0.get()).cmd_flags & ((1 << bindings::REQ_OP_BITS) - 1) }
+    }
+
     /// Complete the request by scheduling `Operations::complete` for
     /// execution.
     ///
